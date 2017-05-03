f2ba6e0001ebe7ecb14f64f32b651bb2f5f363ab
Sam Meech-Ward <sam@meech-ward.com>
2017-05-03 15:04:22 -0700

    Initial Commit

---
+++ Demo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Demo
+//
+//  Created by Sam Meech-Ward on 2017-05-03.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
